c60d2790323498f52af02d5f71450d32ee4d617d
Duy Phan <dpha@netcompany.com>
2022-01-05 17:57:37 +0100

    ExpenseApproval - Start

---
+++ ExpenseApproval/Program.cs
@@ -0,0 +1,20 @@
+﻿using ExpenseApproval;
+
+var manager = new SeniorManager();
+var vp = new VicePresident();
+var coo = new COO();
+
+manager.SetSupervisor(vp);
+vp.SetSupervisor(coo);
+
+var expense = new ExpenseReport("Monitor", 100);
+Console.WriteLine(expense);
+manager.ApproveRequest(expense);
+
+expense = new ExpenseReport("Desk", 900);
+Console.WriteLine(expense);
+manager.ApproveRequest(expense);
+
+expense = new ExpenseReport("Travel", 5_500);
+Console.WriteLine(expense);
+manager.ApproveRequest(expense);
---
+++ ExpenseApproval/SeniorManager.cs
@@ -0,0 +1,67 @@
+﻿namespace ExpenseApproval;
+
+internal record ExpenseReport(string Name, int Amount);
+
+internal interface IManager
+{
+    void SetSupervisor(IManager manager);
+    void ApproveRequest(ExpenseReport expenseReport);
+}
+
+internal class SeniorManager : IManager
+{
+    private IManager? _manager;
+
+    public void ApproveRequest(ExpenseReport expenseReport)
+    {
+        if (expenseReport.Amount < 500)
+        {
+            Console.WriteLine("Approved by SeniorManager");
+            return;
+        }
+        _manager?.ApproveRequest(expenseReport);
+    }
+
+    public void SetSupervisor(IManager manager)
+    {
+        _manager = manager;
+    }
+}
+
+internal class VicePresident : IManager
+{
+    private IManager? _manager;
+
+    public void ApproveRequest(ExpenseReport expenseReport)
+    {
+        if (expenseReport.Amount < 1_000)
+        {
+            Console.WriteLine("Approved by VicePresident");
+            return;
+        }
+        _manager?.ApproveRequest(expenseReport);
+    }
+
+    public void SetSupervisor(IManager manager)
+    {
+        _manager = manager;
+    }
+}
+
+internal class COO : IManager
+{
+    private IManager? _manager;
+
+    public void ApproveRequest(ExpenseReport expenseReport)
+    {
+        if (expenseReport.Amount < 5_000)
+            Console.WriteLine("Approved by COO");
+        else
+            Console.WriteLine("Not approved");
+    }
+
+    public void SetSupervisor(IManager manager)
+    {
+        _manager = manager;
+    }
+}
